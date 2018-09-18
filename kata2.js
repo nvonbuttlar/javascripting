111043c9bf45d0ae0fb61ed87e4c669576ad9c1b
vonbutt <nick.von.buttlar@gmail.com>
2018-09-18 04:24:43 +0000

    New Files, last updates

--- kata2.js
+++ kata2.js
@@ -2,12 +2,19 @@
 // Do not use Array.prototype.filter()
 
 var conditionalSum = function (values, condition) {
-for (var i = 0; var i < values.length; i++) {
-  if (condition === "even") {
+  var target = 0;
+    for (var i = 0; i < values.length; i++) {
+      if (condition === "even" && values[i] % 2 === 0) {
+        target += values[i];
+      } else if (condition === "odd" && values[i] % 2 !== 0) {
+        target += values[i];
+      }
+    } return target;
+  };
 
-  }
 
-}
 
-}
-
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
